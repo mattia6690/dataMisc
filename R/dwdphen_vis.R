6caa6d0b9b04a40ea91d8b30b93864e90aa674dc
ROSSI Mattia <mattia.rossi@ec.europa.eu>
2024-04-15 08:42:14 +0200

    Updated to PhaseIDs

--- R/dwdphen_vis.R
+++ R/dwdphen_vis.R
@@ -7,32 +7,35 @@ library("giscoR")
 library("ggpubr")
 
 nuts_DE0<-gisco_get_countries(country="DE")
-
 phenfiles<-list.files("DWDdata/Phenology/timeseries/",full.names=T)
+refyear<-2023
 
 
 for(i in 1:length(phenfiles)){
   
   cfile<-read_csv(phenfiles[i],show_col_types = FALSE) %>% 
+    mutate(Phase_id=sprintf("%02d",Phase_id)) %>% 
     filter(!is.na(Lat)& !is.na(Lon)) %>% 
-    mutate(BBCHPhase=paste(BBCH_Code,Phase,sep="-")) %>% 
+    mutate(NewPhase=paste(Phase_id,Phase,sep="-")) %>% 
     st_as_sf(coords=c("Lon","Lat"),crs=4326)
   
   # Current -----------------------------------------------------------------
   
-  refyear=year(Sys.Date())
-  refyear=2024
-  
+  if(is.na(refyear)) refyear=year(Sys.Date())
   refyear.stat= refyear-2
-  refyear.mta = refyear-10
   
-  cur<-cfile %>% filter(Referenzjahr==refyear) %>% mutate(year=as.character(Referenzjahr)) %>% 
-    filter(BBCHPhase!="NA-NA")
+  cur<-cfile %>% 
+    filter(Referenzjahr==refyear) %>% 
+    mutate(year=as.character(Referenzjahr)) %>% 
+    filter(NewPhase!="NA-NA")
   
   if(nrow(cur)<1) next
   
-  last5<-cfile %>% filter(Referenzjahr<refyear & Referenzjahr>=refyear.from) %>% select(Stations_id,Stationsname,geometry) %>% unique %>% 
-    mutate(TimeRange=paste(refyear.from,"to",refyear))
+  last5<-cfile %>% 
+    filter(Referenzjahr<refyear & Referenzjahr>=refyear.stat) %>% 
+    select(Stations_id,Stationsname,geometry) %>% 
+    unique %>% 
+    mutate(TimeRange=paste(refyear.stat,"to",refyear))
   
   stat.has<-length(unique(cur$geometry))
   stat.sld<-nrow(last5)
@@ -42,22 +45,23 @@ for(i in 1:length(phenfiles)){
     geom_sf(data=nuts_DE0)+
     geom_sf(data=last5,col="grey70")+
     geom_sf(data=cur,aes(fill=Jultag),shape=21,size=2)+
-    facet_wrap(.~BBCHPhase,ncol=4)+
+    facet_wrap(.~Phase_id,ncol=4)+
     labs(fill="DOY")+
-    scale_fill_gradientn(colors=rainbow(100),limits = c(0, 360),breaks=seq(0,360,90))+
-    ggtitle(paste("Observations",refyear))+
+    scale_fill_gradientn(colors=rainbow(100),limits = c(0, 360),breaks=seq(0,360,30))+
+    ggtitle(paste("Observations",refyear),"Sorted by DWD Phase ID")+
     theme(panel.border=element_blank(),
           panel.grid = element_blank(),
           axis.text.x= element_blank(),
           axis.text.y = element_blank())+
-    theme(legend.position="bottom", legend.box = "horizontal")
-  
+    theme(legend.position="bottom", legend.box = "horizontal",legend.key.width = unit(0.7, "inch"),legend.key.height = unit(0.1, "inch"))
   
   
   # MTA ---------------------------------------------------------------------
   
+  refyear.mta = refyear-10
+  
   lta <- cfile %>% filter(Referenzjahr<refyear & Referenzjahr>=refyear.mta) %>% 
-    group_by(Stations_id,BBCHPhase,geometry) %>% 
+    group_by(Stations_id,NewPhase,geometry) %>% 
     summarize(minDOY=min(Jultag),
               maxDOY=max(Jultag),
               meanDOY=mean(Jultag),
@@ -68,38 +72,44 @@ for(i in 1:length(phenfiles)){
     select(-geometry)
   
   
-  jn<-left_join(cur,as_tibble(lta),by=c("Stations_id","BBCHPhase")) %>% 
+  jn<-left_join(cur,as_tibble(lta),by=c("Stations_id","NewPhase")) %>% 
     filter(!is.na(n)) %>% 
     mutate(Diff=Jultag-meanDOY)
   
+  if(nrow(jn)<1) next
   
   g2<-ggplot()+ theme_light()+
     geom_sf(data=nuts_DE0)+
     geom_sf(data=last5,col="grey70")+
     geom_sf(data=jn,aes(fill=Diff),shape=21,size=2)+
-    facet_wrap(.~BBCHPhase,ncol=4)+
-    labs(fill="Difference in Days")+
-    scale_fill_gradientn(colors=c("Darkgreen","Green","Yellow","Orange","Red"),limits=c(-50,50))+
-    ggtitle(paste("Difference to MTA"))+
+    facet_wrap(.~Phase_id,ncol=4)+
+    labs(fill="Days Difference")+
+    scale_fill_gradientn(colors=c("Blue","Darkgreen","Green","Yellow","Orange","Red","Darkred"),
+                         limits=c(-50,50),breaks=seq(-50,50,10))+
+    ggtitle("Difference to MTA", "Sorted by DWD Phase ID")+
     theme(panel.border=element_blank(),
           panel.grid = element_blank(),
           axis.text.x= element_blank(),
           axis.text.y = element_blank())+
-    theme(legend.position="bottom", legend.box = "horizontal")
+    theme(legend.position="bottom", legend.box = "horizontal",legend.key.width = unit(0.5, "inch"),legend.key.height = unit(0.1, "inch"))
   
   
   # Distribution -------------------------------------------------------------------
   
-  dist <- cfile %>% filter(Referenzjahr>=refyear.mta) %>% mutate(year=as.character(Referenzjahr))
+  dist <- cfile %>% 
+    filter(Referenzjahr>=refyear.mta) %>%
+    mutate(year=as.character(Referenzjahr))
   
-  g3<-ggplot(dist,aes(x=Jultag,fill=BBCHPhase))+ theme_light()+
+  if(nrow(dist)<1) next
+  
+  g3<-ggplot(dist,aes(x=Jultag,fill=NewPhase))+ theme_light()+
     geom_bar()+
     facet_wrap(.~Referenzjahr)+
     xlab("DOY")+
     ylab("Count")+
     ggtitle(paste("DWD Phenology -",unique(cur$Croptype)),
             paste("With Data until", unique(max(cur$Eintrittsdatum))))+
-    labs(fill="<BBCH>-<Phase DE>")+
+    labs(fill="<PhaseID>-<Phase DE>")+
     theme(plot.title = element_text(size = 20, face = "bold"))+
     scale_x_continuous(limits=c(0,360),breaks=seq(0,360,30))+
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
