49c38a229396a6b2636f78809990383a3869e4dd
ROSSI Mattia <mattia.rossi@ec.europa.eu>
2024-04-17 12:14:00 +0200

    Just getting the data and added reporters

--- R/dwdphen_get.R
+++ R/dwdphen_get.R
@@ -64,20 +64,22 @@ dwd.phen.urls<-dwd.phen.url %>%
 
 # Add the name of the ourput
 dwd.phen<- dwd.phen.urls %>% 
-  select(Type1,Type2,Timeframe,L2_URL) %>% 
+  select(reporter,Type1,Type2,Timeframe,L2_URL) %>% 
+  mutate(reporter=str_replace(reporter,"_reporters","")) %>% 
   mutate(Output=paste("DWDdata/Phenology",Type1,Type2,Timeframe,basename(L2_URL),sep="/"))
 
 # Delete the Spezifizierung in Maize
 dwd.phen<-dwd.phen[-which(str_detect(dwd.phen$L2_URL,"Spezifizierung")),]
 
-
-
 # Create the Directories if not present already
 uout<-unique(dirname(dwd.phen$Output))
 s<-sapply(uout,function(x) dir.create(x,recursive=T,showWarnings = F))
 
-# Download ----------------------------------------------------------------
+# Write the DWDphen table
+saveRDS(dwd.phen,"DWDdata/Phenology/DWDphentable.rds")
 
+
+# Download ----------------------------------------------------------------
 for(i in 1:nrow(dwd.phen)){
   
   # Is it an historical tieframe and was it downloaded?
@@ -99,80 +101,6 @@ for(i in 1:nrow(dwd.phen)){
     
   }
 }
-
-
-# Metadata ----------------------------------------------------------------
-# Crops
-
-# List of metadata files
-crop.stations<-"ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/crops/historical/PH_Beschreibung_Phaenologie_Stationen_Jahresmelder.txt"
-crop.phases<-"ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/crops/historical/PH_Beschreibung_Phasendefinition_Jahresmelder_Landwirtschaft_Kulturpflanze.txt"
-fruit.stations<-"ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/fruit/historical/PH_Beschreibung_Phaenologie_Stationen_Jahresmelder.txt"
-fruit.phases<-"ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/fruit/historical/PH_Beschreibung_Phasendefinition_Jahresmelder_Obst.txt"
-
-meta.crops<-bind_cols(Type1="crops",Type2=c("Stations","Phases"),L2_URL=c(crop.stations,crop.phases))
-meta.fruit<-bind_cols(Type1="fruit",Type2=c("Stations","Phases"),L2_URL=c(fruit.stations,fruit.phases))
-
-# Tidy and Output
-meta<-bind_rows(meta.crops,meta.fruit) %>% 
-  mutate(Output=paste0(meta.dir,Type1,"_",Type2,".csv")) %>% 
-  mutate(data=map2(L2_URL,Type2,function(x,y) {
-    
-    # Read and delete White spaces
-    tib<-as_tibble(read.table(x, sep=";", header=TRUE))
-    tib2<-mutate_if(tib,is.character, trimws)
-    
-    # Select the right columns
-    if(y=="Stations") ret<-select(tib2,Stations_id,Stationsname,Lat="geograph.Breite",Lon="geograph.Laenge",Bundesland) 
-    if(y=="Phases") ret<-select(tib2,Objekt_id,Objekt,Phase_id=Phasen_id,Phase,BBCH_Code)
-    
-    return(unique(ret))
-    
-    }))
-
-# Download
-for(i in 1:nrow(meta)) write.csv(meta$data[i],meta$Output[i])
-
-# TimeSeries --------------------------------------------------------------------
-# Tidy the timeseries
-dwd.phen2<-dwd.phen %>% 
-  select(Type1,Type2,Output) %>% 
-  nest(dataRaw=Output)
-
-# Download the timeseries
-for(i in 1:nrow(dwd.phen2)) {
-  
-  # Read
-  l<-lapply(dwd.phen2$dataRaw[[i]]$Output, function(x) read.table(x, sep=";", header=TRUE))
-  
-  
-  #l<-suppressMessages(lapply(dwd.phen2$dataRaw[[i]]$Output, read_csv2))
-  lbind<-unique(do.call(bind_rows,l))
-  lbind.sort<-arrange(lbind,Stations_id,Eintrittsdatum) %>% 
-    mutate(Croptype=dwd.phen2$Type2[i]) %>% 
-    as_tibble %>% 
-    select(-c(eor,X,Eintrittsdatum_QB))
-  
-  
-  # Add metadata
-  stations<-read.csv(paste0("DWDdata/Phenology/metadata/",dwd.phen2$Type1[i],"_Stations.csv"))
-  phases<-read.csv(paste0("DWDdata/Phenology/metadata/",dwd.phen2$Type1[i],"_Phases.csv"))
-  
-  lj1<-left_join(lbind.sort,stations, by = "Stations_id")
-  lj2<-left_join(lj1,phases, by = c("Objekt_id","Phase_id"))
-  
-  # Download
-  out<-paste0(ts.dir,dwd.phen2$Type2[i],"_timeseries_",format(Sys.Date(),"%Y%m%d"),".csv")
-  write_csv(lj2,out)
-  
-  # Housekeeping
-  rm(l,lbind,lbind.sort,lj2)
-  print(paste(out,i))
-  
-}
-
-
-
 
 
 
